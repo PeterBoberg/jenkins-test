f4e86a78b226157fb347b8816b771dbb2e0ffd18
peterboberg <peterb@frost.se>
2018-10-02 22:49:19 +0200

    Add another line of code

--- src/main/java/se/peter/jenkinstest/JenkinsApp.java
+++ src/main/java/se/peter/jenkinstest/JenkinsApp.java
@@ -4,5 +4,6 @@ public class JenkinsApp {
 
     public static void main(String[] args) {
         System.out.println("Im a Jenkins test app");
+        System.out.println("And this is a random change");
     }
 }
