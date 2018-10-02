a004e29e693d4d7e9e2e0b6e6acbbacd1157bca5
peterboberg <peterb@frost.se>
2018-10-02 22:57:48 +0200

    Add more lines of code

--- src/main/java/JenkinsApp.java
+++ src/main/java/JenkinsApp.java
@@ -1,9 +1,8 @@
-package se.peter.jenkinstest;
-
 public class JenkinsApp {
 
     public static void main(String[] args) {
         System.out.println("Im a Jenkins test app");
         System.out.println("And this is a random change");
+        System.out.println("Add yet another line of code");
     }
 }
