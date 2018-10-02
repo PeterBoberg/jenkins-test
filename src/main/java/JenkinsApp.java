31490bc8ea8da0d9178dca9cef958253b70fc19c
peterboberg <peterb@frost.se>
2018-10-02 23:05:48 +0200

    Adding sheep counting functionality

--- src/main/java/JenkinsApp.java
+++ src/main/java/JenkinsApp.java
@@ -4,5 +4,8 @@ public class JenkinsApp {
         System.out.println("Im a Jenkins test app");
         System.out.println("And this is a random change");
         System.out.println("Add yet another line of code");
+        System.out.println("Counting sheeps!");
+        for (int i = 0; i < 10; i++)
+            System.out.println("Sheep no: " + i);
     }
 }
